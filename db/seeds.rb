115ae813a0b878f038f8d69eb1a8c5ac779a21b6
Kosi Akporji <kosiazom@gmail.com>
2020-06-22 17:33:28 -0400

    final push

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,12 @@ Tag.destroy_all
 restau_array = ["Apple Bees", "Olive Garden", "BonChon", "Pizza Hut", "Pop Johns", "Peking Garden", "Nandos", "H Pizza", "Cava", "Silver Diner", "Bukom", "Wendys", "Chickfila", "McDonalds", "Burger King", "Arbys", "In And Out", "Shake Shack", "Sweet Green","Subway"]
 
 created_rest = restau_array.each {|restaurant| Restaurant.create(name: restaurant)}
-
+Restaurant.all.each do |restaurant| 
+dish_array.sample(10).each do|dish_name|
+    Dish.create(name: dish, restaurant: restaurant)
+end
+end
 dish_array = ["Jambalaya", "Pasta", "Rice and Stew", "Pizza", " Hamburger", "French Fries", "Drunken Noodles", "Shrimp and Grits", "Kale Salad", "French Fries"]
 
 dish_array.each {|dish| Dish.create(name: dish, restaurant: Restaurant.all.sample)}
+
